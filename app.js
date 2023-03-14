ca575a6b1809b033ba99d0132f500272360792be
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2023-03-14 22:09:46 +0300

    massive update
    version v.2.3.12

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ toggleBtn.addEventListener('click', ()=>{
   navigation.classList.toggle('active-nav');
   backgroundSvg.classList.toggle('active-svg')
 });
+
+
+const observer = new IntersectionObserver((entries)=> {
+  entries.forEach(entry=>{
+    console.log(entry);
+
+    if (entry.isIntersecting){
+      entry.target.classList.add("show");
+    } else{
+      entry.target.classList.remove("show");      
+    }
+  })
+})
+
+
+
+// grabbing all .hidden elements
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// header oncroll================================
+
+window.addEventListener('scroll', ()=>{
+  document.querySelector(".header").classList.toggle('header_scroll', window.scrollY > 10);
+});
+//Navs Oncroll===============
+window.addEventListener('scroll', ()=>{
+  document.querySelector(".navs").classList.toggle('navy_scroll', window.scrollY > 10);
+});
